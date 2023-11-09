273e0434f9d88b8868641ea4baf0120c10fa9801
Eli-Price <ewillp66@gmail.com>
2023-11-08 18:02:15 -0600

    Improved community gallery

--- Javascript/search.js
+++ Javascript/search.js
@@ -11,23 +11,39 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   // Function to create a figure with an image and caption
-  function createFigure(src, captionText) {
+  function createFigure(src, srcLowRes, captionText) {
     var figure = document.createElement('figure');
     figure.className = 'art-piece'; // Use this class to apply CSS
 
-    var image = new Image();
-    image.src = src;
-    image.alt = captionText;
-    image.className = 'responsive-image'; // Use this class to apply CSS
+    var picture = document.createElement('picture');
+
+    // Low resolution source
+    var sourceLowRes = document.createElement('source');
+    sourceLowRes.srcset = srcLowRes;
+    sourceLowRes.media = '(max-width: 768px)';
+
+    // High resolution source
+    var sourceHighRes = document.createElement('source');
+    sourceHighRes.srcset = src;
+
+    var img = document.createElement('img');
+    img.src = src; // Fallback src if picture element is not supported
+    img.alt = captionText;
+    img.className = 'responsive-image'; // Use this class to apply CSS
+
+    // Append sources and img to the picture element
+    picture.appendChild(sourceLowRes);
+    picture.appendChild(sourceHighRes);
+    picture.appendChild(img);
 
     var caption = document.createElement('figcaption');
     caption.textContent = captionText;
 
-    figure.appendChild(image);
+    figure.appendChild(picture);
     figure.appendChild(caption);
 
     return figure;
-}
+  }
 
   // Fill the gallery with figures (images and captions)
   function fillGallery() {
@@ -36,19 +52,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Image data and captions, would be loaded from a server based on the input but this is just a demonstration
     var imageData = [
-        { src: '../images/highres-henrik-donnestad-t2Sai-AqIpI-unsplash.jpg', caption: 'Photo by Henrik Dønnestad on Unsplash' },
-        { src: '../images/highres-mcgill-library-y4PqRPqSako-unsplash.jpg', caption: 'Photo by McGill Library on Unsplash' },
-        { src: '../images/highres-steve-johnson-e5LdlAMpkEw-unsplash.jpg', caption: 'Photo by Steve Johnson on Unsplash' },
-        
+        { src: '../images/highres/henrik-donnestad-t2Sai-AqIpI-unsplash.jpg', srcLowRes: '../images/lowres/henrik-donnestad-t2Sai-AqIpI-unsplash.jpg', caption: 'Photo by Henrik Dønnestad on Unsplash' },
+        { src: '../images/highres/mcgill-library-y4PqRPqSako-unsplash.jpg', srcLowRes: '../images/lowres/mcgill-library-y4PqRPqSako-unsplash.jpg', caption: 'Photo by McGill Library on Unsplash' },
+        { src: '../images/highres/steve-johnson-e5LdlAMpkEw-unsplash.jpg', srcLowRes: '../images/lowres/steve-johnson-e5LdlAMpkEw-unsplash.jpg', caption: 'Photo by Steve Johnson on Unsplash' },
     ];
 
     // Determine screen size and choose the appropriate image set
-     var imageSet = window.innerWidth <= 768 ? '../images/lowres-' : '../images/highres-';
-    var imageSet = window.innerWidth;
+    //var imageSet = window.innerWidth <= 768 ? '../images/lowres' : '../images/highres';
+    //var imageSet = window.innerWidth;
+    //console.log(imageSet);
 
     // Create and append figure elements to the gallery
     imageData.forEach(function(image) {
-        var figure = createFigure(/*imageSet +*/ image.src, image.caption);
+        var figure = createFigure(image.src, image.srcLowRes, image.caption);
         gallery.appendChild(figure);
     });
   }
@@ -81,5 +97,5 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   // Listen for window resize to update images for responsive layout
-  window.addEventListener('resize', fillGallery);
+  //window.addEventListener('resize', fillGallery);
 });
